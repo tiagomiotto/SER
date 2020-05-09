b5f356653295bf0e4defde3121e7f890233bbe38
tiagomiotto <tiago.miotto@hotmail.com>
2020-05-09 18:29:52 +0100

    test

--- communication/unicast_recv2.c
+++ communication/unicast_recv2.c
@@ -36,30 +36,27 @@
 #include "sys/etimer.h"
 #include "net/ip/uip.h"
 #include "net/ipv6/uip-ds6.h"
+#include "net/ip/uip-debug.h"
 
 #include "simple-udp.h"
-#include "dev/serial-line.h"
+#include "servreg-hack.h"
 
+#include "net/rpl/rpl.h"
 
 #include <stdio.h>
 #include <string.h>
 
 #define UDP_PORT 1234
+#define SERVICE_ID 191
 
-#define SEND_INTERVAL		(20 * CLOCK_SECOND)
+#define SEND_INTERVAL		(10 * CLOCK_SECOND)
 #define SEND_TIME		(random_rand() % (SEND_INTERVAL))
 
-static struct simple_udp_connection broadcast_connection;
-
+static struct simple_udp_connection unicast_connection;
 
 /*---------------------------------------------------------------------------*/
-PROCESS(broadcast_example_process, "UDP broadcast example process");
-PROCESS(handler_proccess, "Serial message handler process");
-PROCESS(test_serial, "Serial line test process");
-
-AUTOSTART_PROCESSES(&handler_proccess,&test_serial);
-
-
+PROCESS(unicast_receiver_process, "Unicast receiver example process");
+AUTOSTART_PROCESSES(&unicast_receiver_process);
 /*---------------------------------------------------------------------------*/
 static void
 receiver(struct simple_udp_connection *c,
@@ -70,71 +67,76 @@ receiver(struct simple_udp_connection *c,
          const uint8_t *data,
          uint16_t datalen)
 {
-  printf("Data received on port %d from port %d with length %d : %s\n",
-         receiver_port, sender_port, datalen, (char *) data);
+  printf("Data received from ");
+  uip_debug_ipaddr_print(sender_addr);
+  printf(" on port %d from port %d with length %d: '%s'\n",
+         receiver_port, sender_port, datalen, data);
 }
 /*---------------------------------------------------------------------------*/
-PROCESS_THREAD(broadcast_example_process, ev, data)
+static uip_ipaddr_t *
+set_global_address(void)
 {
-  static struct etimer periodic_timer;
-  static struct etimer send_timer;
-  uip_ipaddr_t addr;
+  static uip_ipaddr_t ipaddr;
+  int i;
+  uint8_t state;
+
+  uip_ip6addr(&ipaddr, 0xaaaa, 0, 0, 0, 0, 0, 0, 0);
+  uip_ds6_set_addr_iid(&ipaddr, &uip_lladdr);
+  uip_ds6_addr_add(&ipaddr, 0, ADDR_AUTOCONF);
+
+  printf("IPv6 addresses: ");
+  for(i = 0; i < UIP_DS6_ADDR_NB; i++) {
+    state = uip_ds6_if.addr_list[i].state;
+    if(uip_ds6_if.addr_list[i].isused &&
+       (state == ADDR_TENTATIVE || state == ADDR_PREFERRED)) {
+      uip_debug_ipaddr_print(&uip_ds6_if.addr_list[i].ipaddr);
+      printf("\n");
+    }
+  }
+
+  return &ipaddr;
+}
+/*---------------------------------------------------------------------------*/
+static void
+create_rpl_dag(uip_ipaddr_t *ipaddr)
+{
+  struct uip_ds6_addr *root_if;
+
+  root_if = uip_ds6_addr_lookup(ipaddr);
+  if(root_if != NULL) {
+    rpl_dag_t *dag;
+    uip_ipaddr_t prefix;
+    
+    rpl_set_root(RPL_DEFAULT_INSTANCE, ipaddr);
+    dag = rpl_get_any_dag();
+    uip_ip6addr(&prefix, 0xaaaa, 0, 0, 0, 0, 0, 0, 0);
+    rpl_set_prefix(dag, &prefix, 64);
+    PRINTF("created a new RPL dag\n");
+  } else {
+    PRINTF("failed to create a new RPL DAG\n");
+  }
+}
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(unicast_receiver_process, ev, data)
+{
+  uip_ipaddr_t *ipaddr;
 
   PROCESS_BEGIN();
 
-  simple_udp_register(&broadcast_connection, UDP_PORT,
-                      NULL, UDP_PORT,
-                      receiver);
+  servreg_hack_init();
 
+  ipaddr = set_global_address();
 
-  while(1) {
-        PROCESS_WAIT_EVENT_UNTIL(ev = PROCESS_EVENT_CONTINUE);
-    char *msg = (char *) data;
+  create_rpl_dag(ipaddr);
 
-    printf("Sending broadcast %s\n", msg);
-    uip_create_linklocal_allnodes_mcast(&addr);
-    simple_udp_sendto(&broadcast_connection, msg, 5, &addr);
-  }
+  servreg_hack_register(SERVICE_ID, ipaddr);
 
-  PROCESS_END();
-}
-
-PROCESS_THREAD(handler_process, ev, data)
-{
-  static struct etimer periodic_timer;
-  static struct etimer send_timer;
-  uip_ipaddr_t addr;
-
-  PROCESS_BEGIN();
-
+  simple_udp_register(&unicast_connection, UDP_PORT,
+                      NULL, UDP_PORT, receiver);
 
   while(1) {
-    PROCESS_WAIT_EVENT_UNTIL(ev = PROCESS_EVENT_CONTINUE);
-    char *msg = (char *) data;
-
-    if(strcmp(msg,"a"))     process_post(&broadcast_example_process,
-      PROCESS_EVENT_CONTINUE, "tesa");
-    if(strcmp(msg,"b"))     process_post(&broadcast_example_process,
-      PROCESS_EVENT_CONTINUE, "tesb");      
+    PROCESS_WAIT_EVENT();
   }
-
   PROCESS_END();
-  //servreg_hack_item_t *servreg_hack_list_head(void);
 }
-
- PROCESS_THREAD(test_serial, ev, data)
- {
-   PROCESS_BEGIN();
-
-   for(;;) {
-
-    PROCESS_WAIT_EVENT_UNTIL(ev == serial_line_event_message);
-    printf("received line: %s\n", (char *)data);
-    process_post(&handler_process,
-      PROCESS_EVENT_CONTINUE, data);
-   }
-   PROCESS_END();
- }
 /*---------------------------------------------------------------------------*/
-
-
