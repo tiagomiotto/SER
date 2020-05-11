2d99cac0cb03d59a0f50c157c4f537461e22c5d4
tiagomiotto <tiago.miotto@hotmail.com>
2020-05-11 23:35:36 +0100

    s

--- communication/sync_node.c
+++ communication/sync_node.c
@@ -40,26 +40,23 @@
 #include "simple-udp.h"
 #include "dev/serial-line.h"
 
-
 #include <stdio.h>
 #include <string.h>
 
 #define UDP_PORT 1234
 
-#define SEND_INTERVAL		(20 * CLOCK_SECOND)
-#define SEND_TIME		(random_rand() % (SEND_INTERVAL))
+#define SEND_INTERVAL (20 * CLOCK_SECOND)
+#define SEND_TIME (random_rand() % (SEND_INTERVAL))
 
 static struct simple_udp_connection broadcast_connection;
-
 
 /*---------------------------------------------------------------------------*/
 PROCESS(broadcast_example_process, "UDP broadcast example process");
 PROCESS(handler_process, "Serial message handler process");
 PROCESS(test_serial, "Serial line test process");
-PROCESS(network_size, "Network size check periodic process");
+PROCESS(available_nodes_proccess, "Network size check periodic process");
 
-AUTOSTART_PROCESSES(&test_serial,&handler_process);
-
+AUTOSTART_PROCESSES(&test_serial, &handler_process, &available_nodes_proccess);
 
 /*---------------------------------------------------------------------------*/
 static void
@@ -72,7 +69,7 @@ receiver(struct simple_udp_connection *c,
          uint16_t datalen)
 {
   printf("Data received on port %d from port %d with length %d : %s\n",
-         receiver_port, sender_port, datalen, (char *) data);
+         receiver_port, sender_port, datalen, (char *)data);
 }
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(broadcast_example_process, ev, data)
@@ -87,10 +84,10 @@ PROCESS_THREAD(broadcast_example_process, ev, data)
                       NULL, UDP_PORT,
                       receiver);
 
-
-  while(1) {
-        PROCESS_WAIT_EVENT_UNTIL(ev = PROCESS_EVENT_CONTINUE);
-    char *msg = (char *) data;
+  while (1)
+  {
+    PROCESS_WAIT_EVENT_UNTIL(ev = PROCESS_EVENT_CONTINUE);
+    char *msg = (char *)data;
 
     printf("Sending broadcast %s\n", msg);
     uip_create_linklocal_allnodes_mcast(&addr);
@@ -105,20 +102,22 @@ PROCESS_THREAD(handler_process, ev, data)
   static struct etimer periodic_timer;
   static struct etimer send_timer;
   uip_ipaddr_t addr;
-  int i=0;
+  int i = 0;
   PROCESS_BEGIN();
 
-
-  while(1) {
+  while (1)
+  {
     PROCESS_WAIT_EVENT_UNTIL(ev == PROCESS_EVENT_CONTINUE);
-    
-    char *msg = (char *) data;
- 
-    if(strcmp(msg,"info")==0)   {
-      printf("The current state of the system is %s\n", (char *)msg); 
+
+    char *msg = (char *)data;
+
+    if (strcmp(msg, "info") == 0)
+    {
+      printf("The current state of the system is %s\n", (char *)msg);
       continue;
     }
-    else if(strcmp(msg,"command")==0)   {
+    else if (strcmp(msg, "command") == 0)
+    {
       printf("The current nodes are ");
       for (i = 0; i < 4; i++)
       {
@@ -126,18 +125,21 @@ PROCESS_THREAD(handler_process, ev, data)
       }
       printf("select on/off followed by the number of the node\n");
     }
-    else {
+    else
+    {
       printf("Invalid option, use info to get the state of the networrk or command to send commands\n");
       continue;
     }
 
-    
     PROCESS_WAIT_EVENT_UNTIL(ev == PROCESS_EVENT_CONTINUE);
-    msg = (char *) data;
+    msg = (char *)data;
 
-    if(strcmp(msg,"c")==0)    printf("it works1 %s \n",(char *)msg ); 
-    else if(strcmp(msg,"d")==0)   printf("it works2 %s \n",(char *)msg ); 
-    else {
+    if (strcmp(msg, "c") == 0)
+      printf("it works1 %s \n", (char *)msg);
+    else if (strcmp(msg, "d") == 0)
+      printf("it works2 %s \n", (char *)msg);
+    else
+    {
       printf("Invalid option\n");
       continue;
     }
@@ -147,31 +149,38 @@ PROCESS_THREAD(handler_process, ev, data)
   //servreg_hack_item_t *servreg_hack_list_head(void);
 }
 
- PROCESS_THREAD(test_serial, ev, data)
- {
-   PROCESS_BEGIN();
-    printf("Use info to get the state of the network or command to send commands\n");
-   for(;;) {
-
+PROCESS_THREAD(test_serial, ev, data)
+{
+  PROCESS_BEGIN();
+  printf("Use info to get the state of the network or command to send commands\n");
+  for (;;)
+  {
     PROCESS_WAIT_EVENT_UNTIL(ev == serial_line_event_message);
-    printf("received line: %s\n", (char *)data);
     process_post(&handler_process,
-      PROCESS_EVENT_CONTINUE, data);
-   }
-   PROCESS_END();
- }
+                 PROCESS_EVENT_CONTINUE, data);
+  }
+  PROCESS_END();
+}
 
-  PROCESS_THREAD(network_size, ev, data)
- {
-   PROCESS_BEGIN();
 
-   for(;;) {
-
-    PROCESS_WAIT_EVENT_UNTIL(ev == serial_line_event_message);
-
-   }
-   PROCESS_END();
- }
 /*---------------------------------------------------------------------------*/
+PROCESS_THREAD(available_nodes_proccess, ev, data)
+{
+  static struct etimer periodic_timer;
+  uip_ipaddr_t addr;
 
+  PROCESS_BEGIN();
 
+  etimer_set(&periodic_timer, SEND_INTERVAL);
+  while (1)
+  {
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
+    etimer_reset(&periodic_timer);
+    
+    printf("Check nodes\n", msg);
+
+  }
+
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
