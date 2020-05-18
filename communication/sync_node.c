75b7c1159a50b7adafea329882553925491a3368
tiagomiotto <tiago.miotto@hotmail.com>
2020-05-18 21:43:58 +0100

    aa

--- communication/sync_node.c
+++ communication/sync_node.c
@@ -86,8 +86,8 @@ receiver(struct simple_udp_connection *c,
          const uint8_t *data,
          uint16_t datalen)
 {
-  // printf("Data received on port %d from port %d with length %d : %s\n",
-  //        receiver_port, sender_port, datalen, (char *)data);
+  printf("Data received on port %d from port %d with length %d : %s\n",
+         receiver_port, sender_port, datalen, (char *)data);
 }
 
 static uip_ipaddr_t *set_global_address(void)
@@ -150,7 +150,7 @@ void send_command(char* messageTX, uint8_t SERVICE_ID)
     printf("\n");
     strcpy(my_message.msg, messageTX);
     my_message.id=SERVICE_ID;
-    process_post_sync(&unicast_sender_process,
+    process_post(&unicast_sender_process,
                  PROCESS_EVENT_CONTINUE, &my_message);
   }
   else
