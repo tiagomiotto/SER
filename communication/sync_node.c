1e2d2e1290b24e683da429912945430bfa5888e6
Tiago Miotto <tiago.miotto@hotmail.com>
2020-05-19 13:13:07 +0100

    aa

--- communication/sync_node.c
+++ communication/sync_node.c
@@ -328,13 +328,13 @@ PROCESS_THREAD(unicast_sender_process, ev, data)
 
       char buf[sizeof(struct message)];
 
-      memcpy(buf, &my_messageRX, sizeof(my_messageRX));
+      //memcpy(buf, &my_messageRX, sizeof(my_messageRX));
       printf("Sending unicast to ");
       uip_debug_ipaddr_print(addr);
       printf("\n");
       printf("%s",buf);
 
-      simple_udp_sendto(&unicast_connection, buf, strlen(buf) + 1, addr);
+      simple_udp_sendto(&unicast_connection, &my_message, sizeof(struct message) + 1, addr);
     }
     else
     {
