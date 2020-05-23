ef5590118ccc1d7ec87044e7d3a9861476aa3599
tiagomiotto <tiago.miotto@hotmail.com>
2020-05-23 19:07:35 +0100

    aa

--- communication/messaging.c
+++ communication/messaging.c
@@ -102,7 +102,7 @@ uip_ipaddr_t *registerConnection(uint8_t ID)
     if (ID==0) {
         ID=generateID();    
         if(ID==190) create_rpl_dag(ipaddr);
-        printf("My ID is:\n", ID)
+        printf("My ID is:\n", ID);ø
     }
     servreg_hack_register(ID, ipaddr);
 
