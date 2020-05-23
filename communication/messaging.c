9af501617dbf837403a1ea25274fe1b0e4bb5aea
tiagomiotto <tiago.miotto@hotmail.com>
2020-05-23 17:28:58 +0100

    aa

--- communication/messaging.c
+++ communication/messaging.c
@@ -4,23 +4,25 @@ void sendMessage(struct simple_udp_connection connection,
                   struct Message *messageTX)
 {
     uip_ipaddr_t *addr;
-    addr = servreg_hack_lookup(my_messageRX->id);
+    addr = servreg_hack_lookup(messageTX->id);
     if (addr != NULL)
     {
-        my_message.id = ID;
+    
         printf("Sending unicast to ");
         uip_debug_ipaddr_print(addr);
         printf("\n");
-        simple_udp_sendto(&unicast_connection, &my_message, sizeof(struct Message) + 1, addr);
+        simple_udp_sendto(&connection, &messageTX, sizeof(struct Message) + 1, addr);
     }
     else
     {
-        printf("Service %d not found\n", my_messageRX->id);
+        printf("Service %d not found\n", messageTX->id);
     }
 }
 
 struct Message prepareMessage(char* data, uint8_t txID, uint8_t code){
-    struct Message messageTx= {*data,txID};
+    struct Message messageTx;
+    strcpy(messageTx.msg, data);
+    messageTx.id=txID;
     return messageTx;
 }
 
