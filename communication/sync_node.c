8db6cdacd564fe1174fab21d27ae24ffdefaa603
tiagomiotto <tiago.miotto@hotmail.com>
2020-05-30 16:06:47 +0100

    aa

--- communication/sync_node.c
+++ communication/sync_node.c
@@ -197,7 +197,7 @@ PROCESS_THREAD(message_received_handler, ev, data)
     PROCESS_WAIT_EVENT_UNTIL(ev == PROCESS_EVENT_CONTINUE);
     struct Message *my_messageRX = data;
     my_messageRX->destID=my_messageRX->srcID;
-    my_messageRX->srcID=1000;
+    my_messageRX->srcID=200;
     sendMessage(unicast_connection,my_messageRX);
   }
 
