5a19c70eaeaa6fa3299650a1d671da80191f05d9
tiagomiotto <tiago.miotto@hotmail.com>
2020-05-31 02:26:03 +0100

    aa

--- communication/sync_node.c
+++ communication/sync_node.c
@@ -304,7 +304,7 @@ bool updateNodeList_ActiveNode(int nodeID, int state)
       list_add(nodes_list, n);
     }
   }
-  deleteNode(191);
+  purgeNodeList();
 }
 
 void changeNodeSavedState(int nodeID, int state)
