2fc2683096819f43069b6f0b5ca9d160d386e593
tiagomiotto <tiago.miotto@hotmail.com>
2020-05-31 04:58:21 +0100

    hh

--- communication/unicast-sender.c
+++ communication/unicast-sender.c
@@ -68,27 +68,7 @@ receiver(struct simple_udp_connection *c,
          receiver_port, sender_port, datalen);
 }
 /*---------------------------------------------------------------------------*/
-static void
-set_global_address(void)
-{
-  uip_ipaddr_t ipaddr;
-  int i;
-  uint8_t state;
 
-  uip_ip6addr(&ipaddr, 0xaaaa, 0, 0, 0, 0, 0, 0, 0);
-  uip_ds6_set_addr_iid(&ipaddr, &uip_lladdr);
-  uip_ds6_addr_add(&ipaddr, 0, ADDR_AUTOCONF);
-
-  printf("IPv6 addresses: ");
-  for(i = 0; i < UIP_DS6_ADDR_NB; i++) {
-    state = uip_ds6_if.addr_list[i].state;
-    if(uip_ds6_if.addr_list[i].isused &&
-       (state == ADDR_TENTATIVE || state == ADDR_PREFERRED)) {
-      uip_debug_ipaddr_print(&uip_ds6_if.addr_list[i].ipaddr);
-      printf("\n");
-    }
-  }
-}
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(unicast_sender_process, ev, data)
 {
