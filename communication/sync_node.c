70d139c960f318274dcc943a3294e20d81ae7b33
tiagomiotto <tiago.miotto@hotmail.com>
2020-05-31 04:37:04 +0100

    aa

--- communication/sync_node.c
+++ communication/sync_node.c
@@ -81,7 +81,7 @@ void deleteNode(int nodeID);
 void deleteList();
 void purgeNodeList();
 void updateNodesDistances(char* msg);
-struct node* searchInList(int nodeID)
+struct node* searchInList(int nodeID);
 
 // volatile int servHackListSize = 0;
 // volatile int nodesListSize=0;
